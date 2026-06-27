b68654da78a16760da3169fdb56ef716af10bbbb
Nicebott <nikitozierow21@gmail.com>
2026-06-27 13:02:52 -0400

    fix: register service worker immediately in useEffect

--- src/components/ServiceWorkerRegister.tsx
+++ src/components/ServiceWorkerRegister.tsx
@@ -5,16 +5,14 @@ import { useEffect } from 'react';
 export default function ServiceWorkerRegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(
-          function (registration) {
-            console.log('Service Worker registration successful with scope: ', registration.scope);
-          },
-          function (err) {
-            console.log('Service Worker registration failed: ', err);
-          }
-        );
-      });
+      navigator.serviceWorker.register('/sw.js').then(
+        function (registration) {
+          console.log('Service Worker registration successful with scope: ', registration.scope);
+        },
+        function (err) {
+          console.log('Service Worker registration failed: ', err);
+        }
+      );
     }
   }, []);
 
